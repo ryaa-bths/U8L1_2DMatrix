4102db937d7b981032f49a3779503a73f6eae618
ryaa-bths <ryryng75@gmail.com>
2025-03-11 13:58:21 -0400

    Not done with last bit, to be completeed

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,44 @@ public class Main {
         arr1[0][0] = 1;
         arr1[0][1] = 2;
         arr1[0][2] = 3;
+        arr1[1][0] = 4;
+        arr1[1][1] = 5;
+        arr1[1][2] = 6;
+        for (int r = 0; r < arr1.length; r++){
+            System.out.print("[");
+            for (int c = 0; c < arr1[r].length; c++){
+                if (c != arr1[r].length - 1){
+                    System.out.print(arr1[r][c] + ", ");
+                }
+                else{
+                    System.out.print(arr1[r][c]);
+                }
+            }
+            System.out.println("]");
+        }
+        ////
+        arr2[0][0] = 1;
+        arr2[1][0] = 2;
+        arr2[2][0] = 3;
+        arr2[0][1] = 4;
+        arr2[1][1] = 5;
+        arr2[2][1] = 6;
+        for (int r = 0; r < arr2.length; r++){
+            System.out.print("[");
+            for (int c = 0; c < arr2[r].length; c++){
+                if (c != arr2[r].length - 1){
+                    System.out.print(arr2[r][c] + ", ");
+                }
+                else{
+                    System.out.print(arr2[r][c]);
+                }
+            }
+            System.out.println("]");
+        }
 
+        System.out.println(arr1[0][2] + arr2[2][0]);
 
     }
 }
+
+//public void printList(){}//how to make gen method to print array
